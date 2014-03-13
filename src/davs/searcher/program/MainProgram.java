caeddcb3823750485435748a3badcc7715a4fd0d
richard-kiefer <tools@richard.baumhausliebe.de>
2014-03-13 13:28:07 +0100

    Preparatory work. Reformatted.

--- src/davs/searcher/program/MainProgram.java
+++ src/davs/searcher/program/MainProgram.java
@@ -8,48 +8,43 @@ import davs.searcher.tools.IndexUtils;
 import davs.searcher.tools.SpellCheck;
 
 //Entry point to QuickSearch program
-public class MainProgram
-	{
-		//Since spell checking is expensive operation, hold only one copy
-		public static SpellCheck spellCheck;
-		public static boolean isSpellCheckEnabled;
+public class MainProgram {
+    // Since spell checking is expensive operation, hold only one copy
+    public static SpellCheck spellCheck;
+    public static boolean isSpellCheckEnabled;
 
-		
-		public static void main(String[] args)
-			{
-				
-				isSpellCheckEnabled = true;
-				
-				try
-					{
-						spellCheck = new SpellCheck();
-					} catch (IOException e)
-					{
-						//If spellCheck is not initialized, don't even bother with suggestions
-						isSpellCheckEnabled = false;
-						
-						System.err.println("Spell checking is not supported");
-						System.err.println("Make sure file dict is in your working directory");
-					}
-					
-					
-					//First check if index is in current working directory
-					String currentPath = System.getProperty("user.dir");
-				
-					boolean isIndexLocated = false;
-					
-					try
-						{
-							isIndexLocated = IndexUtils.indexExists(currentPath);
-						} catch (IOException e)
-						{
-							isIndexLocated = false;
-						}
-						
-					if(isIndexLocated)
-						new SearchScreen(currentPath).setVisible(true);
-					else new IndexNotFound().setVisible(true);
-						
-			}
+    public static void main(String[] args) {
 
-	}
+        isSpellCheckEnabled = true;
+
+        try {
+            spellCheck = new SpellCheck();
+        } catch (IOException e) {
+            // If spellCheck is not initialized, don't even bother with
+            // suggestions
+            isSpellCheckEnabled = false;
+
+            System.err.println("Spell checking is not supported");
+            System.err
+                    .println("Make sure file dict is in your working directory");
+        }
+
+        // First check if index is in current working directory
+        String currentPath = System.getProperty("user.dir");
+
+        boolean isIndexLocated = false;
+
+        try {
+            isIndexLocated = IndexUtils.indexExists(currentPath);
+        } catch (IOException e) {
+            isIndexLocated = false;
+        }
+
+        if (isIndexLocated)
+            new SearchScreen(currentPath).setVisible(true);
+        else
+            new IndexNotFound().setVisible(true);
+
+    }
+
+}
